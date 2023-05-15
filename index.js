26d1cc9047653f991508ce9ca65c5652447d1425
Stephan Holzschuster <Stephan.Holzschuster@binder-co.com>
2023-05-15 17:03:58 +0200

    refactor index.js

--- index.js
+++ index.js
@@ -1,45 +1,70 @@
+// Initialize page to enable toggle functionality
+function initializePage() {
+    toggleUnitLabel();
+    toggleWeightLabel();
+}
+
 // Dimension Conversion
 function convertLengthValues() {
     let inputValues = document.getElementById("inputValues").value;
     let valuesArray = inputValues.split("x");
-    
-    // Überprüfen, ob das Format korrekt ist
+
+    // Check, if input format is correct
     if (valuesArray.length !== 3 || isNaN(parseFloat(valuesArray[0])) || isNaN(parseFloat(valuesArray[1])) || isNaN(parseFloat(valuesArray[2]))){
-        // Wenn das Format falsch ist, eine Fehlermeldung anzeigen
-        alert("Please enter following format: L x B x H e.g. 10 x 20 x 30", 'danger');
+        alert("Please enter following format: L x B x H - e.g. 10 x 20 x 30", 'danger');
         document.getElementById("outputValues").value = "";
         return;
     } 
     
+    // Grab values from input feild
     let length = parseFloat(valuesArray[0].trim());
     let width = parseFloat(valuesArray[1].trim());
     let height = parseFloat(valuesArray[2].trim());
 
+    let dimConstant = 2.54;
+
     let unitSwitch = document.getElementById("unitSwitch");
     let outputValues = document.getElementById("outputValues");
-    
 
+    // cm to inch
     if (unitSwitch.checked) {
-        let lengthInInch = Math.ceil(length / 2.54);
-        let widthInInch = Math.ceil(width / 2.54);
-        let heightInInch = Math.ceil(height / 2.54);
-
+        let lengthInInch = Math.ceil(length / dimConstant);
+        let widthInInch = Math.ceil(width / dimConstant);
+        let heightInInch = Math.ceil(height / dimConstant);
         outputValues.value = lengthInInch.toFixed(0) + " x " + widthInInch.toFixed(0) + " x " + heightInInch.toFixed(0) + " inches";
-    } else {
-        let lengthInCm = Math.ceil(length * 2.54);
-        let widthInCm = Math.ceil(width * 2.54);
-        let heightInCm = Math.ceil(height * 2.54);
+    }
+    
+    // inch to cm
+    else {
+        let lengthInCm = Math.ceil(length * dimConstant);
+        let widthInCm = Math.ceil(width * dimConstant);
+        let heightInCm = Math.ceil(height * dimConstant);
 
         outputValues.value = lengthInCm.toFixed(0) + " x " + widthInCm.toFixed(0) + " x " + heightInCm.toFixed(0) + " cm";
     }
 }
 
+// EventListeners
+// // EventListener to convert by pressing Enter
 document.addEventListener('keyup', function(event) {
     if (event.key === 'Enter') {
         convertLengthValues();
     }
 });
 
+// // EventListener to delete values by pressing Delete
+function clearValues() {
+    document.getElementById("inputValues").value = "";
+    document.getElementById("outputValues").value = "";
+}
+
+document.addEventListener('keyup', function(event) {
+    if (event.key === 'Delete') {
+        clearValues();
+    }
+});
+
+// Change between units using toggle button
 function toggleUnitLabel() {
     let unitSwitch = document.getElementById("unitSwitch");
     let unitLabel = document.getElementById("unitLabel");
@@ -54,17 +79,6 @@ function toggleUnitLabel() {
     }
 }
 
-function clearValues() {
-    document.getElementById("inputValues").value = "";
-    document.getElementById("outputValues").value = "";
-}
-
-document.addEventListener('keyup', function(event) {
-    if (event.key === 'Delete') {
-        clearValues();
-    }
-});
-
 // Mass Conversion
 function convertWeightValues() {
     let inputValues = document.getElementById("inputWeight").value;
@@ -73,21 +87,38 @@ function convertWeightValues() {
     let unitSwitch = document.getElementById("weightSwitch");
     let outputValues = document.getElementById("outputWeight");
 
+    let weightConstant = 2.20462;
+
     if (unitSwitch.checked) {
-        let weightInKg = Math.ceil(weight * 2.20462);
+        let weightInKg = Math.ceil(weight * weightConstant);
         outputValues.value = weightInKg.toFixed(0) + " lbs";
     } else {
-        let weightInLbs = Math.ceil(weight / 2.20462);
+        let weightInLbs = Math.ceil(weight / weightConstant);
         outputValues.value = weightInLbs.toFixed(0) + " kg";
     }
 }
 
+// EventListeners
+// // EventListener to convert by pressing Enter
 document.addEventListener('keyup', function(event) {
     if (event.key === 'Enter') {
         convertWeightValues();
     }
 });
 
+// // EventListener to delete values by pressing Delete
+function clearWeightValues() {
+    document.getElementById("inputWeight").value = "";
+    document.getElementById("outputWeight").value = "";
+}
+
+document.addEventListener('keyup', function(event) {
+    if (event.key === 'Delete') {
+        clearWeightValues();
+    }
+});
+
+// Change between units using toggle button
 function toggleWeightLabel() {
     let unitSwitch = document.getElementById("weightSwitch");
     let unitLabel = document.getElementById("weightLabel");
@@ -101,18 +132,7 @@ function toggleWeightLabel() {
         inputValuesLabel.innerHTML = "Enter value in lbs:";
     }
 }
-
-function clearWeightValues() {
-    document.getElementById("inputWeight").value = "";
-    document.getElementById("outputWeight").value = "";
-}
-
-document.addEventListener('keyup', function(event) {
-    if (event.key === 'Delete') {
-        clearWeightValues();
-    }
-});
-
+// Jump between dimension input field and mass input field
 var inputValues = document.getElementById("inputValues");
 var inputWeight = document.getElementById("inputWeight");
 
@@ -129,8 +149,3 @@ inputWeight.addEventListener("keydown", function(event) {
     inputValues.focus();
   }
 });
-
-function initializePage() {
-    toggleUnitLabel();
-    toggleWeightLabel();
-}
